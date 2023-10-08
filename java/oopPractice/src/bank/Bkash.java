dee4372598902bcc361702b638db85d56237036a
sabbirpegon <sabbirwalton343@gmail.com>
2023-10-08 16:14:27 +0600

    add

--- java/oopPractice/src/bank/Bkash.java
+++ java/oopPractice/src/bank/Bkash.java
@@ -5,14 +5,19 @@ import java.util.Scanner;
 public class Bkash extends Bank {
     Scanner input = new Scanner(System.in);
     public void  getBkash(){
-        if(this.bankPin!=0){
+        if(this.bankPin==0){
             System.out.println("Enter Bank Name: ");
             String bankName=input.next();
             System.out.println("Enter Bank number:");
             long bankNumber=input.nextLong();
             System.out.println("Enter bank pin:");
-
-            setBank();
+              int bankPin=input.nextInt();
+            System.out.println("Enter Bank total money to deposit:");
+            double bankMoney=input.nextDouble();
+            setBank(bankName,bankNumber,bankPin,bankMoney);
+            loginBank(this.bankPin);
+        }
+        else {
             loginBank(this.bankPin);
         }
     }
