636f2cc8e1091d1dc747ee2a61b287c94668e87c
sabbirpegon <sabbirwalton343@gmail.com>
2023-10-08 15:24:46 +0600

    add

---
+++ java/oopPractice/src/interfaceArea/productInterface.java
@@ -0,0 +1,9 @@
+package interfaceArea;
+
+public interface productInterface {
+
+    public void  setProduct(int productId, String productName, double productCategory,double productPrice, double productTax, double productDiscount );
+    public void  getProduct();
+
+
+}
---
+++ java/oopPractice/src/product/product.java
@@ -0,0 +1,15 @@
+package product;
+
+import interfaceArea.productInterface;
+
+public class product implements productInterface {
+    @Override
+    public void setProduct(int productId, String productName, double productCategory, double productPrice, double productTax, double productDiscount) {
+
+    }
+
+    @Override
+    public void getProduct() {
+
+    }
+}
